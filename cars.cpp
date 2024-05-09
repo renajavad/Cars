5d9440cd8e416b2f011b9d8ed88a155751be898b
Rena <150649018+renajavad@users.noreply.github.com>
2024-05-09 17:39:32 +0400

    Update cars.cpp
    Added checking the number of cars

--- cars.cpp
+++ cars.cpp
@@ -1,66 +1,71 @@
 #include <iostream>
-#include <mutex>
-#include <thread>
 #include <vector>
+#include <mutex>
+#include <chrono>
+#include <thread>
+#include <map>
 
-std::mutex mtx;
+std::mutex m;
+std::map<int, std::vector<std::pair<int, int>>> roadTimes;
 
-void turnGreen(int* p)
-{
-    if (*p == 1)
-    {
-        *p = 2;
-    } 
-    else
-    {
-        *p = 1;
+void turnGreen(int* position) {
+    if (*position == 1) {
+        *position = 2;
+    } else {
+        *position = 1;
     }
 }
 
-void carArrived(int car_id, int road_id, int direct, int* p)
-{
-    std::lock_guard<std::mutex> lg(mtx);
+void carArrived(int carId, int roadId, int direction, int* position) {
+    std::lock_guard<std::mutex> s(m);
     const char* road;
-    if (road_id == 1)
-    {
+    if (roadId == 1) {
         road = "A";
-    }
-    else
-    {
+    } else {
         road = "B";
     }
-    std::cout << "Car " << car_id << " Has Passed Road " << road << " In Direction " << direct << "\n"; 
+    std::cout << "Car " << carId << " has passed road " << road << " in direction " << direction << "\n"; 
 }
 
-int main()
-{
-    std::vector<int> cars = {1, 3, 5, 2, 4};
-    std::vector<int> directions = {2, 1, 2, 4, 3};
-    std::vector<int> arrivalTimes = {10, 20, 30, 40, 50};
+int main() {
+    std::vector<int> cars = {1, 3, 5, 6, 2, 7, 4};
+    std::vector<int> directions = {2, 1, 2, 2, 4, 4, 3};
+    std::vector<int> arrivalTimes = {10, 20, 30, 30, 40, 40, 50};
 
-    int p = 1;
-    for (size_t i = 0; i < cars.size(); ++i)
-    {
-        if (directions[i] <= 2 && p != 1)
-        {
-            p = 1;
+    int position = 1;
+    for (size_t i = 0; i < cars.size(); ++i) {
+        if (directions[i] <= 2 && position != 1) {
+            position = 1;
+        } else if (directions[i] > 2 && position != 2) {
+            position = 2;
         }
-        else if (directions[i] > 2 && p != 2)
-        {
-            p = 2;
+    
+        if (directions[i] <= 2) {
+            if (roadTimes[directions[i]].size() >= 2) {
+                if (roadTimes[directions[i]][0].first == arrivalTimes[i]) {
+                    std::cout << "Invalid data: Two or more cars are trying to exit from road " << directions[i] << " at the same time.\n";
+                    return 1;
+                }
+            }
+            roadTimes[directions[i]].push_back(std::make_pair(arrivalTimes[i], cars[i]));
+        } else {
+            if (!roadTimes[directions[i]].empty()) {
+                if (roadTimes[directions[i]][0].first == arrivalTimes[i]) {
+                    std::cout << "Invalid data: Two or more cars are trying to exit from road " << directions[i] << " at the same time.\n";
+                    return 1;
+                }
+            }
+            roadTimes[directions[i]].push_back(std::make_pair(arrivalTimes[i], cars[i]));
         }
 
         std::this_thread::sleep_for(std::chrono::milliseconds(arrivalTimes[i]));
         
-        carArrived(cars[i], p, directions[i], &p);
+        carArrived(cars[i], position, directions[i], &position);
         
-        if (p == 1 && i + 1 < cars.size() && directions[i + 1] > 2)
-        {
-            std::cout << "Traffic Light On Road B Is Green\n"; 
-        } 
-        else if (p == 2 && i + 1 < cars.size() && directions[i + 1] <= 2)
-        {
-            std::cout << "Traffic Light On Road A Is Green\n";
+        if (position == 1 && i + 1 < cars.size() && directions[i + 1] > 2) {
+            std::cout << "Traffic light on road B is Green\n"; 
+        } else if (position == 2 && i + 1 < cars.size() && directions[i + 1] <= 2) {
+            std::cout << "Traffic light on road A is Green\n";
         }
     }
 
